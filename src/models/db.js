30a8930bd4a2a8e65ac7610b86ebe3f54ebf3e73
ilionroot <ilion.root@gmail.com>
2021-04-30 16:58:48 -0300

    Front-end support

--- src/models/db.js
+++ src/models/db.js
@@ -1,8 +1,11 @@
 const mongoose = require("mongoose");
-mongoose.connect(
-  "mongodb+srv://igus:marina2207@templarios-app-cluster.ewrjn.mongodb.net/templarios-database?retryWrites=true&w=majority",
-  { useNewUrlParser: true, useUnifiedTopology: true }
-);
+
+const url =
+  "mongodb+srv://igus:marina2207@templarios-app-cluster.ewrjn.mongodb.net/templarios-database?retryWrites=true&w=majority";
+// const url =
+//   "mongodb://localhost:27017/templarios-app?readPreference=primary&appname=MongoDB%20Compass&ssl=false";
+
+mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true });
 mongoose.Promise = global.Promise;
 
 let db = mongoose.connection;
